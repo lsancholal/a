56cb2fe7e4769da88fbcad7f2c75c8c3c9136008
sanchola <gmsanches190@gmail.com>
2024-08-14 17:30:08 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
-import Footer from './components/footer'
 import Body from './components/body'
 
 function App() {
